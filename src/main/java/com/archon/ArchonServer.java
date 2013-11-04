740b47453647de04412460de136fb138cf965ab1
EJ Bensing <ebensing@rm-dash-r.com>
2013-11-04 00:49:48 -0500

    Got maven/jetty stuff working

---
+++ src/main/java/com/archon/ArchonServer.java
@@ -0,0 +1,16 @@
+package com.archon;
+
+import org.eclipse.jetty.server.Server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: EJ
+ * Date: 11/4/13
+ * Time: 12:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArchonServer {
+    public static void main(String[] args) {
+
+    }
+}
